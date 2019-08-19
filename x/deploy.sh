0550827cbcbbd10731d3ced001a0cbf2b8185e03
Christian Genco <christian.genco@gmail.com>
2019-08-19 11:37:55 -0400

    initial commit

---
+++ x/deploy.sh
@@ -0,0 +1,12 @@
+yarnpkg run build
+
+# firebase deploy
+
+# NAME="forecast"
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../build" && pwd )"
+
+# aws s3 sync $DIR s3://gen.co/$NAME/ --acl=public-read \
+#   --exclude ".DS_Store" \
+#   --exclude ".git/*"
+
+# aws cloudfront create-invalidation --distribution-id EXW76U2SHH4YQ --paths /$NAME/ /$NAME/index.html /$NAME/favicon.ico
